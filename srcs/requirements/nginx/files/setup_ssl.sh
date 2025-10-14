94a61d30478ab178064bc310a8815f132d06829a
joshw34 <joshwhitley@msn.com>
2025-10-14 16:08:08 +0200

    added nginx.conf

--- srcs/requirements/nginx/files/setup_ssl.sh
+++ srcs/requirements/nginx/files/setup_ssl.sh
@@ -5,4 +5,4 @@ mkdir -p /etc/nginx/ssl
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
   -keyout /etc/nginx/ssl/key.pem \
   -out /etc/nginx/ssl/cert.pem \
-  -subj "/C=FR/ST=PACA/L=Nice/O=42/CN=${DOMAIN_NAME}"
+  -subj "/C=FR/ST=PACA/L=Nice/O=42/CN=$DOMAIN_NAME"
