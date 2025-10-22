3f402688db3441cc3fc5e8492ee55d0f4a2547ea
joshw34 <joshwhitley@msn.com>
2025-10-23 00:54:13 +0200

    ssl added to ftp

--- srcs/requirements/ftp/files/entrypoint.sh
+++ srcs/requirements/ftp/files/entrypoint.sh
@@ -1,29 +1,24 @@
 #!/bin/sh
 FTP_USER_PASSWORD=$(cat /run/secrets/ftp_user_password)
 
-adduser -u 1000 -s /sbin/nologin -D -h /mnt/ftpdata $FTP_USER
+if ! id -u "$FTP_USER" >/dev/null 2>&1; then
+  adduser -u 1000 -s /sbin/nologin -D -h /mnt/ftpdata "$FTP_USER"
+    echo "User $FTP_USER created"
+  else
+    echo "User $FTP_USER already exists"
+fi
 
 # Set password of the user
 echo "$FTP_USER:$FTP_USER_PASSWORD" | chpasswd
+echo "Password set for $FTP_USER"
 
-#cat > /etc/vsftpd/vsftpd.conf <<EOF
-#seccomp_sandbox=NO
-#local_enable=YES
-#write_enable=YES
-#chroot_local_user=YES
-#passwd_chroot_enable=YES
-#allow_writeable_chroot=YES
-#ftpd_banner=Welcome to vsftpd
-#max_clients=10
-#max_per_ip=5
-#local_umask=022
-#pasv_enable=YES
-#pasv_max_port=50010
-#pasv_min_port=50000
-#pasv_address=127.0.0.1
-#anonymous_enable=NO
-#no_anon_password=NO
-#anon_root=/var/ftp
-#EOF
+if [ ! -f /etc/ssl/private/vsftpd.crt ]; then
+  mkdir -p /etc/ssl/private
+  openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout /etc/ssl/private/vsftpd.key \
+    -out /etc/ssl/private/vsftpd.crt \
+    -subj "/C=FR/ST=PACA/L=Nice/O=42/CN=$DOMAIN_NAME"
+  echo "SSL certificate generated"
+fi
 
 exec /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
